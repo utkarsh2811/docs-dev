4af976b9ff29d7334d3ea892872f43f08a7de232
anedosugov <anedosugov@gmail.com>
2019-11-19 17:28:46 +0300

    docker ignore fix

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,15 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-bundle exec middleman build --verbose --clean --build-dir=/usr/src/app/build
+bundle exec middleman build --clean
 
 aws2 s3 sync build s3://dev.intempt.com
-
-pwd
-
-ls
-
-ls build/*
-
-ls ./build/*
 
 aws2 cloudfront create-invalidation --distribution-id E2ETS3E2R2MJV4 --paths "/*"
