660b0db2418105907692988850c43a3cd3fd3f2e
anedosugov <anedosugov@gmail.com>
2019-11-19 16:44:20 +0300

    logs

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -4,4 +4,12 @@ bundle exec middleman build --clean
 
 aws2 s3 sync build s3://dev.intempt.com
 
+pwd
+
+ls
+
+ls build/*
+
+ls ./build/*
+
 aws2 cloudfront create-invalidation --distribution-id E2ETS3E2R2MJV4 --paths "/*"
