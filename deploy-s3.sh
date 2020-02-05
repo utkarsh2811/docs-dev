f78a088d224d6390ed4bcc57beaa26cd2d81d2a7
anedosugov <anedosugov@gmail.com>
2020-02-05 17:32:46 +0300

    s3.sh fix

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -2,6 +2,6 @@
 
 bundle exec middleman build --clean
 
-aws2 s3 sync build s3://dev.intempt.com
+aws s3 sync build s3://dev.intempt.com
 
-aws2 cloudfront create-invalidation --distribution-id E2ETS3E2R2MJV4 --paths "/*"
+aws cloudfront create-invalidation --distribution-id E2ETS3E2R2MJV4 --paths "/*"
