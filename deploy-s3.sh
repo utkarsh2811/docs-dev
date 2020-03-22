0c7be243ea4bac6e2f0573c28a2aa279aee4530f
Andrei Ioksha <efan@bk.ru>
2020-03-22 19:07:31 +0300

    Changed cloudfront distribution id

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -4,4 +4,4 @@ bundle exec middleman build --clean
 
 aws s3 sync build s3://dev.intempt.com
 
-aws cloudfront create-invalidation --distribution-id E2ETS3E2R2MJV4 --paths "/*"
+aws cloudfront create-invalidation --distribution-id EOWUY7D6YABHK --paths "/*"
